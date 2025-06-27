1ff2da0400002f74df27bdfd3ec2a331a0d56418
AsterCasc <astercass@qq.com>
2025-06-27 14:44:37 +0800

    feat: enter app when click message

--- composeApp/src/androidMain/kotlin/ui/components/Notification.android.kt
+++ composeApp/src/androidMain/kotlin/ui/components/Notification.android.kt
@@ -5,6 +5,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -79,12 +80,25 @@ fun createAppNotificationChannel(context: Context) {
 actual fun sendAppNotification(title: String, content: String) {
     val context = MainActivity.mainContext!!
 
+
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+
+
     val builder = NotificationCompat.Builder(context, "TomoyoCommonChannel")
         .setContentTitle(title)
         .setContentText(content)
         .setSmallIcon(android.R.drawable.ic_dialog_info)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
         .setAutoCancel(true)
+        .setContentIntent(pendingIntent)
 
     val notificationManager = context.getSystemService(NotificationManager::class.java)
 
